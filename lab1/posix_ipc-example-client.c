2ea85f920a863a25d9e8a9ce70c613c751a5aa4e
Michael lemito <delovoy.lemit@gmail.com>
2024-10-02 23:43:45 +0300

    first commit

---
+++ lab1/posix_ipc-example-client.c
@@ -0,0 +1,82 @@
+/**
+ * @file posix_ipc-example-client.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-10-02
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+#include <ctype.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stdint.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+float summ(const char* src) {
+  float sum = 0.0;
+  char* endptr;
+
+  while (*src) {
+    while (*src && !isdigit(*src) && *src != '.' && *src != '-' &&
+           *src != '+') {
+      ++src;
+    }
+
+    float value = strtod(src, &endptr);
+    if (endptr != src) {
+      sum += value;
+      src = endptr;
+    } else {
+      ++src;
+    }
+  }
+
+  return sum;
+}
+
+int main(int argc, char** argv) {
+  char buf[4096];
+  ssize_t bytes;
+
+  // pid_t pid = getpid();
+
+  while ((bytes = read(STDIN_FILENO, buf, sizeof(buf)))) {
+    if (bytes < 0) {
+      const char msg[] = "error: failed to read from stdin\n";
+      write(STDERR_FILENO, msg, sizeof(msg));
+      exit(EXIT_FAILURE);
+    } else if (buf[0] == '\n') {
+      // проверка на дурака
+      break;
+    }
+
+    {
+      // заменяем лишним пробел окончанием строки и получаем готовую строчечку
+      buf[bytes - 1] = '\0';
+    }
+
+    {
+      char out_buf[1024];
+      out_buf[0] = '\0';
+      float_t sum = summ(buf);
+      sprintf(out_buf, "%f", sum);
+      int32_t written = write(STDOUT_FILENO, out_buf, strlen(out_buf));
+      if (written == -1) {
+        const char msg[] = "error: failed to write to out buffer\n";
+        write(STDERR_FILENO, msg, sizeof(msg));
+        exit(EXIT_FAILURE);
+      }
+    }
+
+    // TODO: Check for count of actual bytes written
+    const char term = '\0';
+    write(STDOUT_FILENO, &term, sizeof(term));
+  }
+}

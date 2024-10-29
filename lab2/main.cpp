f2f98fae43f80680a0075bc0be75081f0db84740
Michael lemito <delovoy.lemit@gmail.com>
2024-10-29 10:23:08 +0300

    начало работы с колодой + вроде атомик рабочий на самом деле

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -5,35 +5,41 @@
 #include <string.h>
 #include <vector>
 
-#define THREADS_NUM 20
+#define THREADS_NUM 16
 #define DECK_NUM 52
 
 enum CARDS {
-  PICKS,  // пики
-  BUBIS,  // буби
-  CHERVI, // черви
-  CRESTI  // крести
+  PICKS = '1',  // пики
+  BUBIS = '2',  // буби
+  CHERVI = '3', // черви
+  CRESTI = '4'  // крести
 };
 typedef enum CARDS CARDS;
 
+std::atomic<int> ac = 0;
+
 void *just_do(void *args) {
   char DECK[DECK_NUM];
+  for (size_t i = 0; i < DECK_NUM; i++) {
+    DECK[i] = PICKS;
+  }
+  for (size_t i = 0; i < DECK_NUM; i++) {
+    std::cout << DECK[i] << " ";
+  }
   char *input = (char *)args;
+  ac++;
   std::cout << input << std::endl;
   return NULL;
 }
 
 int main() {
-  //   pthread_t threads[THREADS_NUM];
   std::vector<pthread_t> threads(THREADS_NUM);
   char *mewo = strdup("meow");
   for (size_t i = 0; i < THREADS_NUM; i++) {
     pthread_create(&(threads[i]), NULL, just_do, mewo);
   }
 
-  std::atomic<int> ac = 0;
-  ac++;
-  std::cout << ac << std::endl;
+  std::cout <<  "== " << ac << " ==" << std::endl;
 
   return 0;
 }

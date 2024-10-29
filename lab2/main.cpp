ea26cfa123631bf9776a1c87b4deb53c8e880ff5
Michael lemito <delovoy.lemit@gmail.com>
2024-10-29 10:05:33 +0300

    начало работы с картами

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -2,21 +2,33 @@
 #include <iostream>
 #include <pthread.h>
 #include <stdatomic.h>
+#include <string.h>
 #include <vector>
 
 #define THREADS_NUM 20
 #define DECK_NUM 52
 
+enum CARDS {
+  PICKS,  // пики
+  BUBIS,  // буби
+  CHERVI, // черви
+  CRESTI  // крести
+};
+typedef enum CARDS CARDS;
+
 void *just_do(void *args) {
-  std::cout << "Meow" << std::endl;
+  char DECK[DECK_NUM];
+  char *input = (char *)args;
+  std::cout << input << std::endl;
   return NULL;
 }
 
-int main(int argc, const char **argv) {
+int main() {
   //   pthread_t threads[THREADS_NUM];
   std::vector<pthread_t> threads(THREADS_NUM);
+  char *mewo = strdup("meow");
   for (size_t i = 0; i < THREADS_NUM; i++) {
-    pthread_create(&(threads[i]), NULL, just_do, NULL);
+    pthread_create(&(threads[i]), NULL, just_do, mewo);
   }
 
   std::atomic<int> ac = 0;

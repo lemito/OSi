82416ac6b3e1de0fa27f991e07f988c7b7a59372
Michael lemito <delovoy.lemit@gmail.com>
2024-10-29 16:24:45 +0300

    Добавил барьер, обработку ошибки ввода и сделал вывод через write

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,10 +3,14 @@
 #include <pthread.h>
 #include <stdatomic.h>
 #include <string.h>
+#include <unistd.h>
 #include <vector>
 
 #define THREADS_NUM 16
+#define BARRIERS_NUM 16
 #define DECK_NUM 52
+
+pthread_barrier_t barrier;
 
 enum CARDS {
   PICKS = '1',  // пики
@@ -23,23 +27,46 @@ void *just_do(void *args) {
   for (size_t i = 0; i < DECK_NUM; i++) {
     DECK[i] = PICKS;
   }
-  for (size_t i = 0; i < DECK_NUM; i++) {
-    std::cout << DECK[i] << " ";
-  }
+  // for (size_t i = 0; i < DECK_NUM; i++) {
+  //   std::cout << DECK[i] << " ";
+  // }
   char *input = (char *)args;
   ac++;
-  std::cout << input << std::endl;
+  // std::cout << input << std::endl;
   return NULL;
 }
 
-int main() {
+void myCout(char *text) {
+  if (text == NULL) {
+    return;
+  }
+  if (-1 == write(STDOUT_FILENO, text, strlen(text))) {
+    exit(EXIT_FAILURE);
+  }
+  return;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    char BUF[BUFSIZ];
+    sprintf(BUF, "Input error. Use: %s %s\n", argv[0], "<rounds>");
+    myCout(BUF);
+    exit(EXIT_FAILURE);
+  }
+  // количество раундов
+  char *endptr;
+  size_t rounds = strtoull(argv[1], &endptr, 10);
+
   std::vector<pthread_t> threads(THREADS_NUM);
   char *mewo = strdup("meow");
+  pthread_barrier_init(&barrier, NULL, BARRIERS_NUM);
   for (size_t i = 0; i < THREADS_NUM; i++) {
     pthread_create(&(threads[i]), NULL, just_do, mewo);
   }
 
   std::cout << "== " << ac << " ==" << std::endl;
+  pthread_barrier_wait(&barrier);
 
-  return 0;
+  pthread_barrier_destroy(&barrier);
+  return EXIT_SUCCESS;
 }

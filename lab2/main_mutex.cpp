b9d0a1305663f46425d1df61fc8522f198d73f8f
Michael lemito <delovoy.lemit@gmail.com>
2024-11-14 13:14:47 +0300

    реализация на мьютекс теперь полноценный член общества

--- lab2/main_mutex.cpp
+++ lab2/main_mutex.cpp
@@ -1,35 +1,27 @@
 #include <algorithm>
-#include <atomic>
+#include <array>
 #include <iostream>
 #include <mutex>
 #include <pthread.h>
 #include <random>
-#include <shared_mutex>
 #include <stdarg.h>
 #include <string.h>
 #include <unistd.h>
 
-#define print(text, ...)                                                       \
-  do {                                                                         \
-    char BUF[BUFSIZ];                                                          \
-    vsprintf(BUF, text, __VA_ARGS__);                                          \
-    myCout(BUF);                                                               \
-  } while (0)
 #define all(x) x.begin(), x.end()
-#define THREADS_NUM 16
-#define BARRIERS_NUM 16
 #define DECK_NUM 52
 
-std::atomic_int ac{0};
-std::vector<int> deck(DECK_NUM);
-std::mutex m;
+void _print(char *fmt, ...);
+
+int ac = 0;
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 
 void *just_do(void *args) {
   size_t round = *(size_t *)args;
   std::random_device rd;
   std::mt19937 random_generator(rd());
   int local = 0;
-  m.lock();
+  std::array<int, DECK_NUM> deck;
   for (size_t j = 0; j < DECK_NUM; j++) {
     deck[j] = j;
   }
@@ -39,8 +31,15 @@ void *just_do(void *args) {
       ++local;
     }
   }
-  m.unlock();
+  if (0 != pthread_mutex_lock(&m)) {
+    _print("Mutex lock error\n");
+    exit(EXIT_FAILURE);
+  }
   ac += local;
+  if (0 != pthread_mutex_unlock(&m)) {
+    _print("Mutex unlock error\n");
+    exit(EXIT_FAILURE);
+  }
 
   return NULL;
 }
@@ -69,10 +68,11 @@ int main(int argc, char **argv) {
   size_t threads_num = atol(argv[1]);
   size_t rounds = atol(argv[2]);
   size_t rounds_for_thread = rounds / threads_num;
+  if (rounds % threads_num != 0) {
+    threads_num++;
+  }
+  rounds_for_thread = rounds / threads_num;
   std::vector<pthread_t> threads(threads_num);
-  // for (size_t j = 0; j < DECK_NUM; j++) {
-  //   deck[j] = j;
-  // }
   for (size_t i = 0; i < threads_num; i++) {
     if (-1 == pthread_create(&(threads[i]), NULL, just_do,
                              (void *)&rounds_for_thread)) {
@@ -88,21 +88,16 @@ int main(int argc, char **argv) {
     };
   }
 
-  // выводилка удачный попаданий
-  {
-    int b = ac.load();
-    _print("== %d ==\n", b);
-  }
-
   {
     // расчеты
     int total_rounds = rounds; // threads_num * rounds;
-    int f = ac.load();
-    double probability = (double)f / (double)total_rounds;
+    _print("== %d ==\n", ac);
+    double probability = (double)ac / (double)total_rounds;
     char BUF[BUFSIZ];
     sprintf(BUF, "== %f ==\n", probability);
     _print("%s\n", BUF);
   }
 
+  pthread_mutex_destroy(&m);
   return EXIT_SUCCESS;
 }
